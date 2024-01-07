5fc5fb11a2bd882f91abadce4294e635f3478de4
Yusif Eynullabayli <eynullabeyliy@gmail.com>
2024-01-07 17:03:52 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
-const path = require("path");
-
 const nextConfig = {
-  output: 'export',
-  reactStrictMode: false,
-  sassOptions: {
-    includePaths: [path.join(__dirname, "css")],
-  },
-  trailingSlash: true,
-  devIndicators: {
-    buildActivity: false,
-  },
-  eslint: {
-    ignoreDuringBuilds: false,
-  }
-}
+  output: 'standalone',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
