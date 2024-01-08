8292687f3dc953273385786eb3288ff42bb9fd4c
Konul304 <memmedovakonul270@gmail.com>
2024-01-08 09:43:31 +0400

    add sections to home

--- src/app/(dark)/homepage/home1-dark/page.js
+++ src/app/(dark)/homepage/home1-dark/page.js
@@ -5,14 +5,14 @@ import Navbar from '@/components/Common/Navbar';
 import SliderHeader from '@/components/Headers/SliderHeader';
 import AboutUs from '@/components/AboutUs/AboutUs1';
 import Services from '@/components/Services/Services1';
-import Works from '@/components/Works/Works1';
 import Numbers from '@/components/Numbers/Numbers1';
 import Testimonials from '@/components/Testimonials/TestimonialsWithVideo';
-import SkillsCircle from '@/components/Skills/SkillsCircle';
 import Clients from '@/components/Clients/Clients1';
-import Blogs from '@/components/Blogs/Blogs1';
-import CallToAction from '@/components/Others/CallToAction';
 import Footer from '@/components/Common/Footer';
+import Services3 from '@/components/Services/Services3';
+import Works from '@/components/Works/Works1';
+import SkillsCircle from '@/components/Skills/SkillsCircle';
+import FullTestimonials from '@/components/Testimonials/FullTestimonials';
 
 export const metadata = {
   title: 'Vie - Main Demo Dark',
@@ -29,8 +29,9 @@ export default function Home1() {
         <Numbers />
         <Clients theme="dark" />
         <Services />
+        <Services3 />
         <Works />
-        {/* <Testimonials /> */}
+        <FullTestimonials />
         {/* <SkillsCircle theme="dark" />
         <Blogs />
         <CallToAction /> */}
