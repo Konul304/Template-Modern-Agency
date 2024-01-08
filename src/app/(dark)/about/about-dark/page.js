51809801d51bebb8065b816ba6f63ccf8b2a7145
Konul304 <memmedovakonul270@gmail.com>
2024-01-08 08:57:34 +0400

    remove loading from about page

--- src/app/(dark)/about/about-dark/page.js
+++ src/app/(dark)/about/about-dark/page.js
@@ -1,27 +1,27 @@
-import React from "react";
+import React from 'react';
 //= Page components
-import Loading from "@/components/Common/Loader";
-import Navbar from "@/components/Common/Navbar";
-import PagesHeader from "@/components/Headers/PagesHeader";
-import AboutIntro from "@/components/AboutUs/AboutIntro";
-import Services from "@/components/Services/Services1";
-import TestimonialsWithVideo from "@/components/Testimonials/TestimonialsWithVideo";
-import SkillsCircle from "@/components/Skills/SkillsCircle";
-import Team from "@/components/Team/Team1";
-import MinimalArea from "@/components/Others/MinimalArea1";
-import Clients from "@/components/Clients/Clients1";
-import CallToAction from "@/components/Others/CallToAction";
-import Footer from "@/components/Common/Footer";
-import OurHistory from "@/components/OurHistory";
+import Loading from '@/components/Common/Loader';
+import Navbar from '@/components/Common/Navbar';
+import PagesHeader from '@/components/Headers/PagesHeader';
+import AboutIntro from '@/components/AboutUs/AboutIntro';
+import Services from '@/components/Services/Services1';
+import TestimonialsWithVideo from '@/components/Testimonials/TestimonialsWithVideo';
+import SkillsCircle from '@/components/Skills/SkillsCircle';
+import Team from '@/components/Team/Team1';
+import MinimalArea from '@/components/Others/MinimalArea1';
+import Clients from '@/components/Clients/Clients1';
+import CallToAction from '@/components/Others/CallToAction';
+import Footer from '@/components/Common/Footer';
+import OurHistory from '@/components/OurHistory';
 
 export const metadata = {
-  title: "Vie - About Dark",
+  title: 'Vie - About Dark',
 };
 
 function AboutDark() {
   return (
     <>
-      <Loading />
+      {/* <Loading /> */}
       <Navbar />
       <PagesHeader imageLink="/img/slid/about.jpg">
         Our dedicated team of creatives is bursting with talent, experience and
