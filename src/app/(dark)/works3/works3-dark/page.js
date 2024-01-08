36b015c7e920643b0e2487ae5cc635e90686c2fc
Konul304 <memmedovakonul270@gmail.com>
2024-01-08 09:43:46 +0400

    remove loading

--- src/app/(dark)/works3/works3-dark/page.js
+++ src/app/(dark)/works3/works3-dark/page.js
@@ -1,19 +1,18 @@
 import React from 'react';
 //= Page components
-import Loading from '@/components/Common/Loader';
 import Navbar from '@/components/Common/Navbar';
-import WorksHeader from '@/components/Headers/WorksHeader';
-import Works from '@/components/Works/WorksStyle2';
 import Footer from '@/components/Common/Footer';
+import ServicesHeader from '@/components/Headers/ServicesHeader';
+import Services3 from '@/components/Services/Services3';
 
 export const metadata = {
-  title: 'Vie - Works 3 Dark'
-}
+  title: 'Vie - Works 3 Dark',
+};
 
 export default function Works3Page() {
   return (
     <>
-      <Loading />
+      {/* <Loading /> */}
       <div className="circle-bg">
         <div className="circle-color fixed">
           <div className="gradient-circle"></div>
@@ -21,11 +20,12 @@ export default function Works3Page() {
         </div>
       </div>
       <Navbar />
-      <WorksHeader />
+      <ServicesHeader />
       <div className="main-content">
-        <Works grid={2} hideFilter={true} filterPosition="center" />
+        {/* <Works grid={2} hideFilter={true} filterPosition="center" /> */}
+        <Services3 />
         <Footer />
       </div>
     </>
-  )
+  );
 }
