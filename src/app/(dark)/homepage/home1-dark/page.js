46b1055560711d4d1e5d546b75184cd3b05ab62f
Konul304 <memmedovakonul270@gmail.com>
2024-01-09 03:04:16 -0800

    add loading again to pages to prevent delay

--- src/app/(dark)/homepage/home1-dark/page.js
+++ src/app/(dark)/homepage/home1-dark/page.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React from "react";
 //= Page components
-import Loading from '@/components/Common/Loader';
-import Navbar from '@/components/Common/Navbar';
-import SliderHeader from '@/components/Headers/SliderHeader';
-import AboutUs from '@/components/AboutUs/AboutUs1';
-import Services from '@/components/Services/Services1';
-import Numbers from '@/components/Numbers/Numbers1';
-import Partners from '@/components/Clients/Partners';
-import Footer from '@/components/Common/Footer';
-import Works from '@/components/Works/Works1';
-import FullTestimonials from '@/components/Testimonials/FullTestimonials';
+import Loading from "@/components/Common/Loader";
+import Navbar from "@/components/Common/Navbar";
+import SliderHeader from "@/components/Headers/SliderHeader";
+import AboutUs from "@/components/AboutUs/AboutUs1";
+import Services from "@/components/Services/Services1";
+import Numbers from "@/components/Numbers/Numbers1";
+import Partners from "@/components/Clients/Partners";
+import Footer from "@/components/Common/Footer";
+import Works from "@/components/Works/Works1";
+import FullTestimonials from "@/components/Testimonials/FullTestimonials";
 
 export const metadata = {
-  title: 'Vie - Main Demo Dark',
+  title: "Vie - Main Demo Dark",
 };
 
 export default function Home1() {
   return (
     <>
-      {/* <Loading /> */}
+      <Loading />
       <Navbar />
       <SliderHeader />
       <div className="main-content">
