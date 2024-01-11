93a814dcc0000c16db9c5c1569b6e644c7863abd
Konul304 <memmedovakonul270@gmail.com>
2024-01-11 15:54:46 +0400

    create state for inputs in contact form

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 //= Components
 import Split from '@/components/Common/Split';
 //= Static Data
-import contentFormData from "@/data/contact-form.json";
+import contentFormData from '@/data/contact-form.json';
 
 function ContactForm({ theme }) {
+  const [inputValues, setInputValues] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleSubmit = () => {};
   return (
     <section className="contact section-padding">
       <div className="container">
@@ -13,24 +21,62 @@ function ContactForm({ theme }) {
             <div className="form md-mb50">
               <h4 className="fw-700 color-font mb-50">Get In Touch.</h4>
 
-              <form id="contact-form">
+              <form id="contact-form" onSubmit={handleSubmit}>
                 <div className="messages"></div>
 
                 <div className="controls">
                   <div className="form-group">
-                    <input id="form_name" type="text" name="name" placeholder="Name" required="required" />
+                    <input
+                      id="form_name"
+                      type="text"
+                      name="name"
+                      placeholder="Name"
+                      required="required"
+                      onChange={(e) =>
+                        setInputValues((prevState) => ({
+                          ...prevState,
+                          name: e?.target?.value,
+                        }))
+                      }
+                    />
                   </div>
 
                   <div className="form-group">
-                    <input id="form_email" type="email" name="email" placeholder="Email" required="required" />
+                    <input
+                      id="form_email"
+                      type="email"
+                      name="email"
+                      placeholder="Email"
+                      required="required"
+                      onChange={(e) =>
+                        setInputValues((prevState) => ({
+                          ...prevState,
+                          email: e?.target?.value,
+                        }))
+                      }
+                    />
                   </div>
 
                   <div className="form-group">
-                    <textarea id="form_message" name="message" placeholder="Message" rows="4"
-                      required="required"></textarea>
+                    <textarea
+                      id="form_message"
+                      name="message"
+                      placeholder="Message"
+                      rows="4"
+                      required="required"
+                      onChange={(e) =>
+                        setInputValues((prevState) => ({
+                          ...prevState,
+                          message: e?.target?.value,
+                        }))
+                      }
+                    ></textarea>
                   </div>
 
-                  <button type="submit" className={`butn ${theme === 'light' ? 'dark' : 'bord'}`}>
+                  <button
+                    type="submit"
+                    className={`butn ${theme === 'light' ? 'dark' : 'bord'}`}
+                  >
                     <span>Send Message</span>
                   </button>
                 </div>
@@ -41,7 +87,9 @@ function ContactForm({ theme }) {
             <div className="cont-info">
               <h4 className="fw-700 color-font mb-50">Contact Info.</h4>
               <Split>
-                <h3 className="wow" data-splitting>{contentFormData.title}</h3>
+                <h3 className="wow" data-splitting>
+                  {contentFormData.title}
+                </h3>
               </Split>
               <div className="item mb-40">
                 <h5>
@@ -50,7 +98,9 @@ function ContactForm({ theme }) {
                 <h5>{contentFormData.phone}</h5>
               </div>
               <Split>
-                <h3 className="wow" data-splitting>Visit Us.</h3>
+                <h3 className="wow" data-splitting>
+                  Visit Us.
+                </h3>
               </Split>
               <div className="item">
                 <h6>
@@ -78,7 +128,7 @@ function ContactForm({ theme }) {
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default ContactForm
+export default ContactForm;
