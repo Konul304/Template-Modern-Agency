540ddba6f1b7bd26bc9d09abe182ba2042330cab
Konul304 <memmedovakonul270@gmail.com>
2024-01-22 14:19:49 +0400

    complete integration of partners api

--- src/app/(dark)/homepage/home1-dark/page.js
+++ src/app/(dark)/homepage/home1-dark/page.js
@@ -1,25 +1,26 @@
-import React from "react";
+import React from 'react';
 //= Page components
-import Loading from "@/components/Common/Loader";
-import Navbar from "@/components/Common/Navbar";
-import SliderHeader from "@/components/Headers/SliderHeader";
-import AboutUs from "@/components/AboutUs/AboutUs1";
-import Services from "@/components/Services/Services1";
-import Numbers from "@/components/Numbers/Numbers1";
-import Partners from "@/components/Clients/Partners";
-import Footer from "@/components/Common/Footer";
-import Works from "@/components/Works/Works1";
-import FullTestimonials from "@/components/Testimonials/FullTestimonials";
-import Cases from "@/components/Blogs/Cases";
-import Services1 from "@/components/Services/Services1";
-import { getAbout } from "@/app/(api)/api";
+import Loading from '@/components/Common/Loader';
+import Navbar from '@/components/Common/Navbar';
+import SliderHeader from '@/components/Headers/SliderHeader';
+import AboutUs from '@/components/AboutUs/AboutUs1';
+import Services from '@/components/Services/Services1';
+import Numbers from '@/components/Numbers/Numbers1';
+import Partners from '@/components/Clients/Partners';
+import Footer from '@/components/Common/Footer';
+import Works from '@/components/Works/Works1';
+import FullTestimonials from '@/components/Testimonials/FullTestimonials';
+import Cases from '@/components/Blogs/Cases';
+import Services1 from '@/components/Services/Services1';
+import { getAbout, getPartners } from '@/app/(api)/api';
 
 export const metadata = {
-  title: "Vie - Main Demo Dark",
+  title: 'Vie - Main Demo Dark',
 };
 
 const Home1 = async () => {
   const response = await getAbout();
+  const partnerData = await getPartners();
   return (
     <>
       <Loading />
@@ -28,7 +29,7 @@ const Home1 = async () => {
       <div className="main-content">
         <AboutUs data={response} />
         <Numbers />
-        <Partners theme="dark" />
+        <Partners theme="dark" partners={partnerData} />
         {/* <Services /> */}
         <Services1 style="4item" />
         {/* <Services3 /> */}
--- src/components/Clients/Partners.jsx
+++ src/components/Clients/Partners.jsx
@@ -1,147 +1,148 @@
-"use client";
-import React from "react";
-//= Components
-import Split from "@/components/Common/Split";
+'use client';
+import Split from '@/components/Common/Split';
 //= Static Data
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Autoplay } from 'swiper';
 //= Scripts
-import ClientsData from "@/data/sections/clients.json";
 
-function Clients({ theme }) {
-  const first = ClientsData.slice(0, ClientsData.length / 2);
-  const second = ClientsData.slice(4, ClientsData.length);
-  
-const swiperOptions = {
-  modules: [Autoplay, Navigation],
-  loop: true,
-  navigation: {
-    prevEl: ".arrows .prev",
-    nextEl: ".arrows .next",
-  },
-  centeredSlides: true,
-  autoplay: {
-    delay: 6000, // Set the autoplay delay to 5000 milliseconds (5 seconds)
-    disableOnInteraction: false, // Allow autoplay to continue after user interaction
-  },
-  slidesPerView: 1,
-  spaceBetween: 60,
-  breakpoints: {
-    1024: {
-      slidesPerView: 1,
-      centeredSlides: false,
+const Clients = ({ theme, partners }) => {
+  const swiperOptions = {
+    modules: [Autoplay, Navigation],
+    loop: true,
+    navigation: {
+      prevEl: '.arrows .prev',
+      nextEl: '.arrows .next',
     },
-    767: {
-      slidesPerView: 1,
-      centeredSlides: false,
+    centeredSlides: true,
+    autoplay: {
+      delay: 6000, // Set the autoplay delay to 5000 milliseconds (5 seconds)
+      disableOnInteraction: false, // Allow autoplay to continue after user interaction
     },
-    480: {
-      slidesPerView: 1,
-      centeredSlides: false,
-    }
-  },
-}
-
+    slidesPerView: 1,
+    spaceBetween: 60,
+    breakpoints: {
+      1024: {
+        slidesPerView: 1,
+        centeredSlides: false,
+      },
+      767: {
+        slidesPerView: 1,
+        centeredSlides: false,
+      },
+      480: {
+        slidesPerView: 1,
+        centeredSlides: false,
+      },
+    },
+  };
   return (
     <section className="clients section-padding">
       <h1 className="mb-50 text-center fw-600">Our Partners</h1>
-           <div className="container-fluid position-re">
+      <div className="container-fluid position-re">
         <div className="row wow fadeInUp" data-wow-delay=".5s">
-        <div className="col-lg-12">
-           <Swiper {...swiperOptions} className="slic-item" data-wow-delay=".5s">
+          <div className="col-lg-12">
+            <Swiper
+              {...swiperOptions}
+              className="slic-item"
+              data-wow-delay=".5s"
+            >
               <SwiperSlide className="item">
-            <div>
-              <div className="row bord">
-                {first.map((item) => (
-                  <div key={item.id} className="col-md-3 col-6 brands">
-                    <div
-                      className="item wow fadeIn"
-                      data-wow-delay={`${item.id == 1
-                        ? ".3"
-                        : item.id == 2
-                          ? ".6"
-                          : item.id == 3
-                            ? ".8"
-                            : item.id == 4
-                              ? ".3"
-                              : ""
-                        }s`}
-                    >
-                      <div className="img">
-                        {theme === "light" ? (
-                          <img src={item.lightImage} alt="" />
-                        ) : (
-                          <img src={item.darkImage} alt="" />
-                        )}
-                        <Split>
-                          <a
-                            href="#0"
-                            className="link words chars splitting"
-                            data-splitting
+                <div>
+                  <div className="row bord">
+                    {partners?.map((item) => {
+                      const logo_url =
+                        'https://project141.s3.eu-north-1.amazonaws.com/' +
+                        item?.logoLink;
+                      return (
+                        <div key={item.id} className="col-md-3 col-6 brands">
+                          <div
+                            className="item wow fadeIn"
+                            data-wow-delay={`${
+                              item.id == 1
+                                ? '.3'
+                                : item.id == 2
+                                ? '.6'
+                                : item.id == 3
+                                ? '.8'
+                                : item.id == 4
+                                ? '.3'
+                                : ''
+                            }s`}
                           >
-                            {item.url}
-                          </a>
-                        </Split>
-                      </div>
-                    </div>
+                            <div className="img">
+                              <img src={logo_url} alt="" />
+                              <Split>
+                                <a
+                                  href="#0"
+                                  className="link words chars splitting"
+                                  data-splitting
+                                >
+                                  {item.link}
+                                </a>
+                              </Split>
+                            </div>
+                          </div>
+                        </div>
+                      );
+                    })}
                   </div>
-                ))}
-              </div>
-              <div className="row">
-                {second.map((item) => (
-                  <div
-                    key={item.id}
-                    className={`${item.id == 5
-                      ? "col-md-3 col-6 brands sm-mb30"
-                      : item.id == 6
-                        ? "col-md-3 col-6 brands sm-mb30"
-                        : item.id == 7
-                          ? "col-md-3 col-6 brands"
-                          : item.id == 8
-                            ? "col-md-3 col-6 brands"
-                            : ""
-                      }`}
-                  >
-                    <div
-                      className="item wow fadeIn"
-                      data-wow-delay={`${item.id == 1
-                        ? ".4"
-                        : item.id == 2
-                          ? ".7"
-                          : item.id == 3
-                            ? ".5"
-                            : item.id == 4
-                              ? ".3"
-                              : ""
-                        }s`}
-                    >
-                      <div className="img">
-                        {theme === "light" ? (
-                          <img src={item.lightImage} alt="" />
-                        ) : (
-                          <img src={item.darkImage} alt="" />
-                        )}
-                        <Split>
-                          <a
-                            href="#0"
-                            className="link words chars splitting"
-                            data-splitting
-                          >
-                            {item.url}
-                          </a>
-                        </Split>
+                  {/* <div className="row">
+                    {second.map((item) => (
+                      <div
+                        key={item.id}
+                        className={`${
+                          item.id == 5
+                            ? 'col-md-3 col-6 brands sm-mb30'
+                            : item.id == 6
+                            ? 'col-md-3 col-6 brands sm-mb30'
+                            : item.id == 7
+                            ? 'col-md-3 col-6 brands'
+                            : item.id == 8
+                            ? 'col-md-3 col-6 brands'
+                            : ''
+                        }`}
+                      >
+                        <div
+                          className="item wow fadeIn"
+                          data-wow-delay={`${
+                            item.id == 1
+                              ? '.4'
+                              : item.id == 2
+                              ? '.7'
+                              : item.id == 3
+                              ? '.5'
+                              : item.id == 4
+                              ? '.3'
+                              : ''
+                          }s`}
+                        >
+                          <div className="img">
+                            {theme === 'light' ? (
+                              <img src={item.lightImage} alt="" />
+                            ) : (
+                              <img src={item.darkImage} alt="" />
+                            )}
+                            <Split>
+                              <a
+                                href="#0"
+                                className="link words chars splitting"
+                                data-splitting
+                              >
+                                {item.url}
+                              </a>
+                            </Split>
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
+                    ))}
+                  </div> */}
+                </div>
               </SwiperSlide>
             </Swiper>
           </div>
-          </div>
+        </div>
 
-            <div className="arrows">
+        <div className="arrows">
           <div className="button_container">
             <div className="next cursor-pointer ">
               <span className="pe-7s-angle-right"></span>
@@ -151,7 +152,7 @@ const swiperOptions = {
             </div>
           </div>
         </div>
-        </div>
+      </div>
     </section>
   );
 };
