ded0f58beacd03eb60bb7e7aff4150bdb7aacb9c
Konul304 <memmedovakonul270@gmail.com>
2024-03-18 14:23:46 +0400

    light background

--- src/components/Team/Team2.jsx
+++ src/components/Team/Team2.jsx
@@ -5,6 +5,42 @@ import styles from '../../styles/Cases.module.scss';
 function Team2(data) {
   const img_url =
     'https://project141.s3.eu-north-1.amazonaws.com/' + data?.data?.imageLink;
+
+  console.log(data);
+  const swiperOptions = {
+    modules: [Autoplay, Navigation],
+    loop: true,
+    navigation: {
+      prevEl: '.arrows .prev',
+      nextEl: '.arrows .next',
+    },
+    centeredSlides: true,
+    autoplay: {
+      delay: 6000, // Set the autoplay delay to 5000 milliseconds (5 seconds)
+      disableOnInteraction: false, // Allow autoplay to continue after user interaction
+    },
+    slidesPerView: 1,
+    spaceBetween: 60,
+    breakpoints: {
+      1024: {
+        slidesPerView: 1,
+        centeredSlides: false,
+      },
+      767: {
+        slidesPerView: 1,
+        centeredSlides: false,
+      },
+      480: {
+        slidesPerView: 1,
+        centeredSlides: false,
+      },
+    },
+  };
+  console.log();
+  const imagesPerPage = 4; // Number of partners to render per SwiperSlide
+  const totalSlides = Math.ceil(
+    data?.data?.teamMembersImages?.length / imagesPerPage
+  );
   return (
     <section className="team section-padding">
       <div className="container">
@@ -24,6 +60,78 @@ function Team2(data) {
           <div className="col-lg-11">
             <div className="post">
               <img src={img_url} alt="" />
+              <div
+                className="row wow fadeInUp"
+                data-wow-delay=".5s"
+                style={{ marginTop: '70px', marginBottom: '70px' }}
+              >
+                <div className="col-lg-12">
+                  <Swiper
+                    {...swiperOptions}
+                    className="slic-item"
+                    data-wow-delay=".5s"
+                  >
+                    {Array.from({ length: totalSlides }, (_, slideIndex) => (
+                      <SwiperSlide className="item" key={slideIndex}>
+                        <div className="row bord">
+                          {data?.data?.teamMembersImages
+                            ?.slice(
+                              slideIndex * imagesPerPage,
+                              (slideIndex + 1) * imagesPerPage
+                            )
+                            .map((member, index) => (
+                              <div
+                                key={index + 1}
+                                className="col-md-3 col-6 brands"
+                              >
+                                <div
+                                  className="item wow fadeIn"
+                                  data-wow-delay={`${
+                                    index === 1
+                                      ? '.3'
+                                      : index === 2
+                                      ? '.6'
+                                      : index === 3
+                                      ? '.8'
+                                      : index === 4
+                                      ? '.3'
+                                      : ''
+                                  }s`}
+                                >
+                                  <div className="img">
+                                    <img
+                                      src={`https://project141.s3.eu-north-1.amazonaws.com/${member}`}
+                                      alt=""
+                                    />
+                                    {/* <Split>
+                                      <a
+                                        href={`${partner?.link}`}
+                                        className="link words chars splitting"
+                                        data-splitting
+                                      >
+                                        {partner.link}
+                                      </a>
+                                    </Split> */}
+                                  </div>
+                                </div>
+                              </div>
+                            ))}
+                        </div>
+                      </SwiperSlide>
+                    ))}
+                  </Swiper>
+                </div>
+              </div>
+              <div className="arrows">
+                <div className="button_container">
+                  <div className="next cursor-pointer ">
+                    <span className="pe-7s-angle-right"></span>
+                  </div>
+                  <div className="prev cursor-pointer">
+                    <span className="pe-7s-angle-left"></span>
+                  </div>
+                </div>
+              </div>
               <div className={`${styles.team_info} text-center mt-5`}>
                 {data?.data?.description &&
                   HTMLReactParser(data?.data?.description)}
